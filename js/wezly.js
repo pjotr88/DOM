08dec798cbd74dcc24b1fc6822b1cc14d737614f
Piotr Chojnowski <piotr.chojnowsky@gmail.com>
2017-04-02 11:27:31 +0200

    węzły

---
+++ js/wezly.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var pierwszyDiv = document.getElementById('parFirst');
+console.log(pierwszyDiv);
+//console.log(pierwszyDiv.parentNode);
+//console.log(pierwszyDiv.parentElement);
+
+//console.log(pierwszyDiv.childNodes);
+//console.log(pierwszyDiv.childNodes[1]);
+//console.log(pierwszyDiv.children);
+//console.log(pierwszyDiv.firstChild);
+//console.log(pierwszyDiv.lastChild);
+
+var pierwszyChild = pierwszyDiv.childNodes[0];
+console.log(pierwszyChild);
+console.log(pierwszyChild.nextSibling);
